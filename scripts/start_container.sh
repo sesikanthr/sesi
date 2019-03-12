2487e943a89f03b5cf358f1c1aec70e918a17d93
dedadhic <dadhich.dev@gmail.com>
2019-03-12 17:15:54 +0530

    docker deployement  change

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-
+echo "Logging in to Amazon ECR..."
 $(aws ecr get-login --no-include-email --region us-west-2)
+echo "Running docker image"
 sudo docker run -d --name nms-dashboard -p 8443:8443 860360332628.dkr.ecr.us-west-2.amazonaws.com/nms-dashboard
