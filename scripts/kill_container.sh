f0be02243266358ec01c73d5990cd3844f837161
dedadhic <dadhich.dev@gmail.com>
2019-03-12 18:51:43 +0530

    correcting buildspec.yml

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-echo "Logging in to Amazon ECR..."
-pwd
-$(aws ecr get-login --no-include-email --region us-west-2)
+
 echo "removing exiting docker image"
 docker stop nms-dashboard || true && docker rm -f nms-dashboard || true
