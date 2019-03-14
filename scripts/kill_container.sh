b60ebe3389fa8cea4422a91fda562e17fbcc37a3
dedadhic <dadhich.dev@gmail.com>
2019-03-14 23:13:54 +0530

    removing docker img

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "removing exiting docker image"
-docker stop nms-dashboard || true && docker rm -f nms-dashboard || true
+docker stop nms-dashboard || true && docker rm -f nms-dashboard || true && docker rmi 860360332628.dkr.ecr.us-west-2.amazonaws.com/nms-dashboard || true
