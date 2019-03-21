a6bcd5fd091c254db7b7e4d15a0b039da2c9ce73
dedadhic <dadhich.dev@gmail.com>
2019-03-21 07:13:38 +0530

    pushing latest changes

--- scripts/kill_container.sh
+++ scripts/kill_container.sh
@@ -2,5 +2,7 @@
 
 set -e
 
-echo "removing exiting docker image"
+echo "removing exiting docker container"
 sudo docker stop cdr-dashboard || true && docker rm -f cdr-dashboard || true
+echo "removing exiting docker image"
+sudo docker images | grep "cdr-dashboard" | awk '{print $1":"$2}' | xargs docker rmi
